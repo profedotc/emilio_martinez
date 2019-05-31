6df7d44616bb98aad5d2e31534dedee6e39c4495
Emilio Martinez Rivero <emiliomartinezriv@gmail.com>
2019-05-31 17:32:36 +0200

    Se soluciona error al inicializar struct
    Se estaba declarando struct gol *g en vez de struct gol g.
    Esto puede de dar problemas pues nuestro puntero no "apunta a nada".

--- main.c
+++ main.c
@@ -6,14 +6,14 @@
 int main()
 {
 	int it = 0;
-	struct gol *g;
-	gol_init(g);
+	struct gol g;
+	gol_init(&g);
 	do
 	{
 		printf("\033cIteration %d\n", it);
-		gol_print(g);
+		gol_print(&g);
 
-		gol_step(g);
+		gol_step(&g);
 		++it;
 	} while (getchar() != 'q');
 
